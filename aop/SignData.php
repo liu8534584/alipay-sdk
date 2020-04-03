6be56fa6a9d9c8af40c3ce0c867928221b2e031d
溜达 <liuda@xiaoshijie.com>
2020-04-03 13:33:11 +0800

    add

---
+++ aop/SignData.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiehua
+ * Date: 15/5/2
+ * Time: 下午6:21
+ */
+
+namespace Alipay;
+class SignData
+{
+
+    public $signSourceData = null;
+
+
+    public $sign = null;
+
+} 
